815e9cb7f99c44ec416f7896973a90623be2ccaa
Amit pandey <amitpandeye120@gmail.com>
2024-11-07 08:33:27 +0530

    Amit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import  './App.css';
+import React,{useState} from 'react';
 
 function App() {
+const[Status,setStatus]=React.useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        Status?<h1>Hello Amit</h1>:null
+      }
+    {/* <h1>Hide and Show Button</h1> */}
+
+
+    {/* <p> when we perfrom operation of hide and show on two different button </p>
+    <button onClick={()=>setStatus(false)}>Hide</button>  
+    <button onClick={()=>setStatus(true)}>Show</button>
+ */}
+
+
+<p>when we perform operation of hide and show on same button  (Toggle)</p>
+<button onClick={()=>setStatus(!Status)}>Toggle</button>
+
+
+
     </div>
   );
 }
